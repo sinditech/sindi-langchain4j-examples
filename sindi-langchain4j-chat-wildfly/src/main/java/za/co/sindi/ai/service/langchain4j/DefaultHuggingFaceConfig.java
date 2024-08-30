0b1aeff5381a5e7e9ff03e7aee9c3ebfe5813667
Buhake Sindi <buhake.sindi@sindi.co.za>
2024-08-31 00:03:59 +0200

    Examples added for Payara Micro, Wildfly and OpenLiberty.

--- sindi-langchain4j-chat-wildfly/src/main/java/za/co/sindi/ai/service/langchain4j/DefaultHuggingFaceConfig.java
+++ sindi-langchain4j-chat-wildfly/src/main/java/za/co/sindi/ai/service/langchain4j/DefaultHuggingFaceConfig.java
@@ -20,7 +20,7 @@ public class DefaultHuggingFaceConfig implements HuggingFaceChatLanguageConfig, 
 	@Override
 	public String getAccessToken() {
 		// TODO Auto-generated method stub
-		return "<ENTER YOUR ACCESS TOKEN HERE, OR INJECT YOUR MICROPROFILE CONFIG HERE>";
+		return "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";
 	}
 
 	/* (non-Javadoc)
