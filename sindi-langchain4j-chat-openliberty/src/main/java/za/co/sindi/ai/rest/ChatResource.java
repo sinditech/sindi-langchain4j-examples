7ecfee96016a12b6760e28792819e260531efd95
Buhake Sindi <buhake.sindi@sindi.co.za>
2024-12-17 00:11:28 +0200

    Feature add/update.

--- sindi-langchain4j-chat-openliberty/src/main/java/za/co/sindi/ai/rest/ChatResource.java
+++ sindi-langchain4j-chat-openliberty/src/main/java/za/co/sindi/ai/rest/ChatResource.java
@@ -9,6 +9,8 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import za.co.sindi.ai.service.langchain4j.Assistant;
+import za.co.sindi.ai.service.langchain4j.FraudAiService;
+import za.co.sindi.ai.service.langchain4j.FraudResponse;
 
 /**
  * @author Buhake Sindi
@@ -21,6 +23,10 @@ public class ChatResource {
 //	private Instance<Assistant> assistant;
 	private Assistant assistant;
 	
+	@Inject
+//	private Instance<Assistant> assistant;
+	private FraudAiService fraudService;
+	
 	@GET
 	@Operation(
 	        summary = "Chat to our friendly assistant."
@@ -30,5 +36,12 @@ public class ChatResource {
 	public String chat(@QueryParam("message") String message) {
 //        return assistant.get().chat(message);
         return assistant.chat(message);
+    }
+	
+	@GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/fraud")
+    public FraudResponse detectFraudForCustomer(@QueryParam("name") String name, @QueryParam("surname") String surname) {
+        return fraudService.detectFraudForCustomer(name, surname);
     }
 }
